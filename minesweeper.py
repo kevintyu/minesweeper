4e3e6651bd8f8f4b46f34cc7eb4cdc82b4c9d6f5
Dudrop <du.yuechen.kelvin@gmail.com>
2020-04-02 15:13:38 -0400

    updated makeBoard() to initialize a board size GRID_SIZE

--- minesweeper.py
+++ minesweeper.py
@@ -1,27 +1,39 @@
 #kelvin du
 #101152192
+'''
 
+'''
 def placeMines():
-    print()
+    grid = [[0 for j in range(GRID_SIZE)] for i in range(GRID_SIZE)]
+    for row in range(0, GRID_SIZE):
+        for col in range(0, GRID_SIZE):
+            grid[row][col] = ""
     return grid
 
 def makeBoard():
+    grid = [[0 for j in range(GRID_SIZE)] for i in range(GRID_SIZE)]
+    for row in range(0, GRID_SIZE):
+        for col in range(0, GRID_SIZE):
+            grid[row][col] = "#"
     return grid
 
-def showBoard(2dlist):
+def showBoard(board):
     print()
 
-def countHiddenCells():
+def countHiddenCells(matrix):
     print()
 
-def countAllMines():
+def countAllMines(matrix):
     print()
 
-def isMineAt():
+def isMineAt(matrix):
     print()
 
-def countAdjacentMines():
+def countAdjacentMines(matrix):
     print()
 
 def main():
-    print()
+    print(placeMines())
+GRID_SIZE = 5
+MINE_CHANCE = 10
+main()
