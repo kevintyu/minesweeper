7cce7c7c484cc4ff35d0c9d8276f8bb6fa9a2387
Dudrop <du.yuechen.kelvin@gmail.com>
2020-04-02 14:25:29 -0400

    added file

---
+++ minesweeper.py
@@ -0,0 +1,27 @@
+#kelvin du
+#101152192
+
+def placeMines():
+    print()
+    return grid
+
+def makeBoard():
+    return grid
+
+def showBoard(2dlist):
+    print()
+
+def countHiddenCells():
+    print()
+
+def countAllMines():
+    print()
+
+def isMineAt():
+    print()
+
+def countAdjacentMines():
+    print()
+
+def main():
+    print()
